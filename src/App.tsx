a2bdf58e76554d30b077a3fe6080da8a10474595
unknown <yigitarsland@icloud.com>
2025-04-24 21:08:04 +0200

    Add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className='test-5xl text-orange-500 font-bold'>Welcome to Website!</h1>
+    <h1 className='text-5xl text-orange-500 font-bold font-circular-web'>
+      Welcome to Website!
+    </h1>
   )
 }
 
